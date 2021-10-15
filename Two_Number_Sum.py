ecc8051413212a1d8b7ffe4d0505ed1851daf442
Zhaoyang Chen <31273076+CZhaoYoung@users.noreply.github.com>
2021-10-14 23:16:05 -0400

    Update Two_Number_Sum.py

--- Two_Number_Sum.py
+++ Two_Number_Sum.py
@@ -1,12 +1,32 @@
 
+# comments on solution 2
+# nums is an empty dic at first. The first num in the result will be mismatched.
+# And then the second on will be matched
 def Solution2(array, targetSum):
     nums = {}
     for num in array:
-        secondSum = targetSum - num
-        if secondSum in nums:
-            return [secondSum, num]
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
         else:
             nums[num] = True
+    return []
+
+# comments
+# preprocess the input may reduce the time complexity
+# sth like the union find in MST. Sorting the edge in advance will reduce Kruskal algorithm to O(nlog(n))
+def Solution3(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right += 1
     return []
 
 
@@ -26,4 +46,5 @@ def twoNumberSum(array, targetSum):
 if __name__ == '__main__':
     array = [3, 5, -4, 8, 11, 1, -1, 6]
     targetSum = 10
-    print(twoNumberSum(array, targetSum))
+    print(Solution3(array, targetSum))
+    print(2)
