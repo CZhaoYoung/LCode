41e899fa92a57765cbda8f71a04f086677ba11b1
Zhaoyang Chen <31273076+CZhaoYoung@users.noreply.github.com>
2021-11-02 13:27:45 -0400

    Create Product_Sum.py

---
+++ Product_Sum.py
@@ -0,0 +1,13 @@
+def productSum(array, multiplier = 1):
+    sum = 0
+    for element in array:
+        if type(element) is list:
+            sum += productSum(element, multiplier + 1)
+        else:
+            sum += element
+    return sum * multiplier
+
+
+if __name__ == '__main__':
+    array = [5, 2, [7, -1], 3, [6, [-13, 8],4]]
+    print(productSum(array))
