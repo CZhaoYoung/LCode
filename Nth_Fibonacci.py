bc3c6e14b095616a55c6cbe0af0e58cbe64cafbe
Zhaoyang Chen <31273076+CZhaoYoung@users.noreply.github.com>
2021-11-02 13:00:01 -0400

    Create Nth_Fibonacci.py

---
+++ Nth_Fibonacci.py
@@ -0,0 +1,10 @@
+def getNthFib(n):
+    if n == 2:
+        return 1
+    elif n == 1:
+        return 0
+    else:
+        return getNthFib(n-1) + getNthFib(n-2)
+
+if __name__ == '__main__':
+    print(getNthFib(10))
